f1019ae4c0e918922a230ac982f1556beb2af3df
Afaan Bilal <afaanbilal@gmail.com>
2021-01-24 14:37:07 +0530

    📝 Added reference docs

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,44 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\AfaanBilal\\RandomString\\RandomString",
+            "name": "RandomString",
+            "summary": "",
+            "url": "classes/AfaanBilal-RandomString-RandomString.html"
+        },                {
+            "fqsen": "\\AfaanBilal\\RandomString\\RandomString\u003A\u003A__construct\u0028\u0029",
+            "name": "__construct",
+            "summary": "Constructor",
+            "url": "classes/AfaanBilal-RandomString-RandomString.html#method___construct"
+        },                {
+            "fqsen": "\\AfaanBilal\\RandomString\\RandomString\u003A\u003Agenerate\u0028\u0029",
+            "name": "generate",
+            "summary": "Generate\u0020a\u0020random\u0020string.",
+            "url": "classes/AfaanBilal-RandomString-RandomString.html#method_generate"
+        },                {
+            "fqsen": "\\AfaanBilal\\RandomString\\RandomString\u003A\u003A\u0024length",
+            "name": "length",
+            "summary": "String\u0020length",
+            "url": "classes/AfaanBilal-RandomString-RandomString.html#property_length"
+        },                {
+            "fqsen": "\\AfaanBilal\\RandomString\\RandomString\u003A\u003A\u0024charset",
+            "name": "charset",
+            "summary": "The\u0020charset",
+            "url": "classes/AfaanBilal-RandomString-RandomString.html#property_charset"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\AfaanBilal\\RandomString",
+            "name": "RandomString",
+            "summary": "",
+            "url": "namespaces/afaanbilal-randomstring.html"
+        },                {
+            "fqsen": "\\AfaanBilal",
+            "name": "AfaanBilal",
+            "summary": "",
+            "url": "namespaces/afaanbilal.html"
+        }            ]
+);
